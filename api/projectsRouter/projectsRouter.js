19e64e7adf4539e32dcff75b6259ca269670256d
Josh Z <jzieger10@gmail.com>
2019-01-11 11:33:42 -0500

    Made some edits to wording, all project endpoints functional

--- api/projectsRouter/projectsRouter.js
+++ api/projectsRouter/projectsRouter.js
@@ -1,25 +1,25 @@
 const express = require("express");
 
-const postDb = require("../../data/helpers/projectModel.js");
+const projectModel = require("../../data/helpers/projectModel.js");
 
 const router = express.Router();
 
 router
 	.route("/")
 	.get((req, res) => {
-		postDb
+		projectModel
 			.get()
-			.then(posts => {
-				res.status(200).json({ posts });
+			.then(projects => {
+				res.status(200).json({ projects });
 			})
 			.catch(res.status(500));
 	})
 	.post((req, res) => {
 		let newPost = req.body;
-		postDb
+		projectModel
 			.insert(newPost)
-			.then(posts => {
-				res.status(201).json({ posts });
+			.then(projects => {
+				res.status(201).json({ projects });
 			})
 			.catch(res.status(500));
 	});
@@ -28,17 +28,26 @@ router
 	.route("/:id")
 	.get((req, res) => {
 		const id = req.params.id;
-		postDb
+		projectModel
 			.get(id)
-			.then(post => {
-				res.status(201).json({ post });
+			.then(project => {
+				res.status(201).json({ project });
+			})
+			.catch(res.status(500));
+    })
+    .get((req, res) => {
+		const id = req.params.id;
+		projectModel
+			.getProjectActions(id)
+			.then(project => {
+				res.status(201).json({ project });
 			})
 			.catch(res.status(500));
 	})
 	.put((req, res) => {
 		const id = req.params.id;
 		const changes = req.body;
-		postDb
+		projectModel
 			.update(id, changes)
 			.then(count => {
 				res.status(201).json({ count });
@@ -47,7 +56,7 @@ router
 	})
 	.delete((req, res) => {
 		const id = req.params.id;
-		postDb
+		projectModel
 			.remove(id)
 			.then(count => {
 				res.status(200).json({ count });
